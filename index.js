4e08b35b22644e11aba2fd8a1af6cae0985bd8cd
theophildio <dtheo.sangma@gmail.com>
2022-05-22 12:51:48 +0600

    Server created for Cordless Tools Terminal

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello Cordless Tools Terminal Server!')
+})
+
+app.listen(port, () => {
+  console.log(`Cordless Tools Terminal listening on port: ${port}`)
+})
