03790a6ce50ef23ff64f0b23c8c8d3fb1c39419e
theophildio <dtheo.sangma@gmail.com>
2022-05-25 16:23:55 +0600

    Set reverse function to fetch all tools

--- index.js
+++ index.js
@@ -11,18 +11,33 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hcmia.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// JWT Token
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if(!authHeader) {
+    return res.status(401).send({message: '401 Unauthorized access'});
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+    if(err) {
+      return res.status(403).send({message: '403 Forbidden access'});
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 async function run() {
   try {
     await client.connect();
     // Data collections
     const toolsCollection = client.db('cordlessTools').collection('tools');
-    const purchaseCollection = client.db('cordlessTools').collection('purchase');
+    const userCollection = client.db('cordlessTools').collection('users');
 
     // Store all tools
     app.get('/tool', async (req, res) => {
       const query = {};
-      const tools = await toolsCollection.find(query).toArray();
+      const tools = await (await toolsCollection.find(query).toArray()).reverse();
       res.send(tools);
     });
     // Get tool by ID
