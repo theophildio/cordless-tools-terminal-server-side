4a3a661185a3ca20e64cc7441be420e7c02203d4
theophildio <dtheo.sangma@gmail.com>
2022-06-19 22:26:11 +0600

    Delete and update tool api created

--- index.js
+++ index.js
@@ -62,25 +62,30 @@ async function run() {
       res.send(purchase);
     });
     // Update tool
-    app.put('/tool/:id', verifyJWT, verifyAdmin, async(req, res) => {
+    app.put('/tool/:id', verifyJWT, async(req, res) => {
       const id = req.params.id;
       const filter = {_id: ObjectId(id)};
+      const updateTool = req.body;
       const options = {upsert: true};
       const updateDoc = {$set: updateTool};
       const result = await toolsCollection.updateOne(filter, updateDoc, options);
-      const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '5h'})
-      res.send({result, token});
+      res.send(result);
     });
     // Add new Tool
 		app.post('/tool/additem', verifyJWT, verifyAdmin, async (req, res) => {
-			const email = req.body;
 			const addTool = req.body;
-			const filter = {email: email};
 			const options = {upsert: true};
 			const updateDoc = {$set: addTool};
-			const result = await toolsCollection.updateOne(filter, updateDoc, options);
+			const result = await toolsCollection.updateOne(updateDoc, options);
 			res.send(result);
 		});
+    // Delete tool 
+    app.delete('/tool/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: ObjectId(id)};
+      const result = await toolsCollection.deleteOne(filter);
+      res.send(result);
+    })
     // Post all users from signup form
     app.post('/user/:email', async (req, res) => {
       const email = req.params.email;
